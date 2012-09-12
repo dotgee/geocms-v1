981174098f76f47e5810bbc840f31fc9926112c9
Jerome Chapron <jchapron@dotgee.fr>
2012-09-12 16:30:48 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geocms2::Application.config.secret_token = '8251d1fc88a21e014cb5e771478e75144d76edccc0b5e4f4fee75baf06413309840c4b391997a670a22cda88e4d2ea5907c1344ffeef0023549a39b99b3124b3'
