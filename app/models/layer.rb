cfb124a56eb314a1c93c81c71e53c1cf1437e9e4
Antoine Tanguy <atanguy@dotgee.fr>
2013-01-28 15:52:50 +0100

    mise a jour du context

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -13,7 +13,7 @@ class Layer < ActiveRecord::Base
 
   has_and_belongs_to_many :categories
   belongs_to :data_source
-  has_many :contexts_layers
+  has_many :contexts_layers, :dependent => :destroy, :uniq => true
   has_many :contexts, :through => :contexts_layers
   has_many :dimensions
 
