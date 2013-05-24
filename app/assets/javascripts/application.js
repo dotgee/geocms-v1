197c0b5dc9f2644e8a8cb8a7350f7e54b55878ee
Jerome Chapron <jchapron@dotgee.fr>
2013-05-24 14:30:48 +0200

    loading bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,24 @@ $("a").click(function(e){
   }
 })
 
+$(document).ajaxStart(function() {
+    startProgress()
+});
+
+function startProgress() {
+  if ($("#progress").length === 0) {
+    $("body").append($("<div><dt/><dd/></div>").attr("id", "progress"));
+    $("#progress").width((50 + Math.random() * 30) + "%");
+  }
+};
+
+$(document).ajaxComplete(function() {
+    stopProgress();
+});
+
+function stopProgress () {
+  //End loading animation
+  $("#progress").width("101%").delay(200).fadeOut(400, function() {
+    $(this).remove();
+  });
+}
